df048aab91d5a4919589795fdc4ee7e5b83c3f5e
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-07-01 15:28:44 -0300

    initial mui config

---
+++ src/app/themes/index.ts
@@ -0,0 +1,16 @@
+"use client";
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2500FF",
+      dark: "#1900b2",
+      light: "#5033ff",
+    },
+
+    background: {
+      default: "#f7f6f3",
+    },
+  },
+});
